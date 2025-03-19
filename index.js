5bced674b9565b0c3a7e7f8541ff33a7b5dced5a
VNWabule <vicwabule@gmail.com>
2025-03-19 18:55:45 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(Response => Response.json())
+    .then(data => {
+        document.body.innerHTML = data.id.toString()
+        return data
+    })
+    .catch(error => {
+        document.body.innerHTML = error.message
+        return error.message
+    })
+}
